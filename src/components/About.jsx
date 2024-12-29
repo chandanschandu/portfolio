465358cdd142b11702744bd359534ece280a2088
chandan s <chandans7711@gmail.com>
2024-12-29 00:38:56 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -55,6 +55,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >I'm a dedicated software developer with a deep interest in web technologies and a strong foundation in languages like Python, JavaScript, and frameworks such as React, Django, and Node.js. I’m always eager to learn and collaborate, ensuring that the solutions I develop are not only scalable and efficient but also user-friendly. Let’s work together to transform your ideas into practical, real-world applications!
+      >A day without git push is like a symphony without music—utterly incomplete!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
